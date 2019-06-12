d21387076a194f4e5ac72b79ade8f9983bb87cfa
torque19 <tusharjha118@gmail.com>
2019-06-12 11:46:39 +0530

    multiplication added

--- calculator.c
+++ calculator.c
@@ -1,25 +1,24 @@
 #include<stdio.h>
-#include<conio.h>
 void main()
 {
-double a, b, result;
+int a, b, result;
 int choice;
-clrscr();
 result = 0;
-printf("Enter two nubers");
-scanf("%lf%lf", &a,&b);
+printf("Enter two numbers");
+scanf("%d%d", &a,&b);
 printf("Enter your choice: \n 1 for adition\n 2 for subtraction\n 3 for multiplication");
 scanf("%d", &choice);
 switch(choice){
 case 1: result = a+b;
-	printf("The sum of %lf and %lf is: %lf",a,b,result);
+	printf("The sum of %d and %d is: %d",a,b,result);
 	break;
 case 2: result = a-b; 
-	printf("The difference of %lf and %lf is: %lf",a,b,result);
+	printf("The difference of %d and %d is: %d",a,b,result);
 	break;
-case 3: break;
+case 3: result = a*b;
+        printf("The multiplication of %d and %d is:%d",a,b,result);
+        break;
 default:"invalid choice";
 }
-getch();
 }
 
